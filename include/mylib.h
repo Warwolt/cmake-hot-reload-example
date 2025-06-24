e8c583bb9e4c4abfbaa5259635af0f8873224df3
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-06-24 09:41:25 +0200

    Support both DLL and LIB builds

--- include/mylib.h
+++ include/mylib.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef _DEBUG
+#ifdef MYLIB_SHARED
     #ifdef EXPORT_MYLIB
         #define MYLIB_API __declspec(dllexport)
     #else
@@ -8,16 +8,6 @@
     #endif
 #else
     #define MYLIB_API
-#endif
-
-#ifdef _DEBUG
-    #ifdef EXPORT_MYLIB
-        #define MYLIB_API_STR "__declspec(dllexport)"
-    #else
-        #define MYLIB_API_STR "__declspec(dllimport)"
-    #endif
-#else
-    #define MYLIB_API_STR ""
 #endif
 
 extern "C" MYLIB_API void hello();
