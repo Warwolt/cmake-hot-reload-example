21a5f8b7580df5fa317bb36992268e595b31b13b
Rasmus Källqvist <rkallqvist@microsoft.com>
2025-06-29 18:20:13 +0200

    Button press

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "mylib.h"
 
 #include <stdint.h>
+#include <stdio.h>
 #include <windows.h>
 
 int64_t get_pc_frequency() {
@@ -9,8 +10,7 @@ int64_t get_pc_frequency() {
 	return frequency.QuadPart; // ticks per second
 }
 
-// milliseconds
-int64_t get_ticks() {
+int64_t time_now_ms() {
 	static int64_t pc_freq = get_pc_frequency();
 	LARGE_INTEGER ticks;
 	QueryPerformanceCounter(&ticks);
@@ -19,13 +19,18 @@ int64_t get_ticks() {
 
 int main() {
 	int number = 1;
-	int64_t time_start = get_ticks();
+	int64_t time_start = time_now_ms();
+	bool prev_button_state = false;
 	while (true) {
-		int64_t time_now = get_ticks();
+		int64_t time_now = time_now_ms();
 		int64_t elapsed_time = time_now - time_start;
 
-		// if keyboard key pressed
-		// print "key pressed"
+		bool button_state = GetKeyState(VK_F10) & 0x8000;
+		bool button_pressed_now = button_state && ! prev_button_state;
+		prev_button_state = button_state;
+		if (button_pressed_now) {
+			printf("F10 pressed\n");
+		}
 
 		if (elapsed_time >= 1000) {
 			time_start = time_now;
